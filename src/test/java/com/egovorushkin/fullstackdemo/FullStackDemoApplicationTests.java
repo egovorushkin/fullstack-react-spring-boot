b5f612320a4c27b2ec291913b163a3fdb8c44750
Evgenii Govorushkin <e.govorushkin@gmail.com>
2021-04-11 21:39:58 +0300

    fullstack-react-spring-boot

---
+++ src/test/java/com/egovorushkin/fullstackdemo/FullStackDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egovorushkin.fullstackdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
